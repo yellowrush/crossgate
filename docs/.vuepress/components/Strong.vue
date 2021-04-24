56c7753329743cba6f853a7cee8937a9b454798d
yellowrush <yellowrush001@gmail.com>
2021-04-24 15:58:02 +0900

    修复

---
+++ docs/.vuepress/components/Strong.vue
@@ -0,0 +1,15 @@
+<template>
+  <strong>
+    <slot />
+  </strong>
+</template>
+
+<script>
+export default {
+  name: 'Strong',
+};
+</script>
+<style lang="stylus" scoped>
+  strong
+    color var(--accentColor)
+</style>
