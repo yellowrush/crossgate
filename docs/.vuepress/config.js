a0b8d79b78ab84dec4e3381e022ede1550abd18a
yellowrush <yellowrush001@gmail.com>
2021-04-28 12:38:46 +0900

    修改confi

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,7 +3,7 @@ module.exports = {
   title: '魔力玩家的百科全书 - 魔力全书',
   description: '魔力玩家的百科全书',
   head: [
-    ['link', { rel: "apple-touch-icon", sizes: "180x180", href: "apple-touch-icon.png"}],
+    ['link', { rel: "apple-touch-icon", sizes: "180x180", href: "apple-touch-icon-180x180.png"}],
     ['link', { rel: "icon", type: "image/png", sizes: "32x32", href: "favicon-32x32.png"}],
     ['link', { rel: "icon", type: "image/png", sizes: "16x16", href: "favicon-16x16.png"}],
     ['link', { rel: "manifest", href: "site.webmanifest"}],
