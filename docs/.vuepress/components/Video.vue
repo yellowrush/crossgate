2646d12a0f217e65fef71262b2ecdaf08bd31958
yellowrush <yellowrush001@gmail.com>
2021-04-29 12:39:41 +0900

    新增饰品

---
+++ docs/.vuepress/components/Video.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="video">
+    <iframe
+      class="video-player"
+      :src="src"
+      scrolling="no"
+      border="0"
+      frameborder="no"
+      framespacing="0"
+      allowfullscreen="false">
+      </iframe>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Video',
+  props: {
+    src: {
+      type: String,
+      default: '',
+    },
+  },
+};
+</script>
+<style lang="stylus" scoped>
+@require '../styles/config'
+@require '../styles/fallback'
+
+  .video
+    .video-player
+      width 100%
+      height 490px
+      margin 1rem 0
+
+@media (max-width: $MQMobile)
+  .video
+    .video-player
+      height 50vw
+</style>
