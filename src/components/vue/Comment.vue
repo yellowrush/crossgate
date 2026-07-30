e9b821eabf5d89d84a48d77646d2ca3dddd92f66
yellowrush <yellowrush001@gmail.com>
2026-07-30 14:51:31 +0900

    Add site manifest and icons; update Comment component script loading logic

--- src/components/vue/Comment.vue
+++ src/components/vue/Comment.vue
@@ -6,15 +6,17 @@
 import { onMounted } from 'vue';
 
 onMounted(() => {
-  const script = document.createElement('script');
-  script.type = 'text/javascript';
-  script.async = true;
-  script.setAttribute('issue-term', 'pathname');
-  script.setAttribute('theme', 'github-dark');
-  script.setAttribute('repo', 'crossgate-book/crossgate-book.github.io');
-  script.crossOrigin = 'anonymous';
-  script.src = 'https://beaudar.lipk.org/client.js';
-  document.getElementById('comment').appendChild(script);
+  if (import.meta.env.PROD) {
+    const script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.async = true;
+    script.setAttribute('issue-term', 'pathname');
+    script.setAttribute('theme', 'github-dark');
+    script.setAttribute('repo', 'crossgate-book/crossgate-book.github.io');
+    script.crossOrigin = 'anonymous';
+    script.src = 'https://beaudar.lipk.org/client.js';
+    document.getElementById('comment').appendChild(script);
+  }
 });
 </script>
 
